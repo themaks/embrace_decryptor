bbfca637be566411f4cc8553c9fa58d355bb55e1
Maxime Meignan <meignanmaxime@hotmail.fr>
2018-06-12 08:27:58 -0400

    general refactoring

--- decrypt_file.py
+++ decrypt_file.py
@@ -1,5 +1,5 @@
 from Crypto.Cipher import AES
-import os,string,math,sys,shutil
+import os,string,math,sys,shutil,argparse
 holdrand = None
 alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 CRYPTED_EXTENSION = ".[embrace@airmail.cc].embrace"
@@ -7,12 +7,6 @@ POSTFIX_SIZE = 512
 ENCRYPTION_SIZE_LIMIT = 0x100000
 SPECIAL_EXTENTIONS = [".sql", ".mdf", ".txt", ".dbf", ".ckp", ".dacpac", ".db3", ".dtxs", ".mdt", ".sdf", ".MDF", ".DBF"]
 
-def print_help():
-    print "Usage: %s <filename> [time of the encryption in seconds]" % sys.argv[0]
-    print
-    print "To work, the last 16 characters of the encrypted file's path (including the file's name) must be the same as when the file was encrypted"
-    print "If this condition is not met, only the 16 first bytes of the file at most will be destroyed"
-    
 
 def uint32(n):
     return n & 0xFFFFFFFF
@@ -27,7 +21,7 @@ def bytes_to_str(b):
 
 def srand(seed):
     global holdrand
-    holdrand = uint32(seed) 
+    holdrand = uint32(seed)
 
 def rand():
     global holdrand
@@ -43,7 +37,7 @@ def init_keys(time):
         tmp = (0x842108421084211 * r) >> 64 # tmp is 32 bits
         random_32_chars_id.append(alphabet[r - 62 * ((tmp + (uint64(r-tmp) >> 1)) >> 5)])
     return "".join(random_32_chars_id)
-    
+
 def pad_or_trunk(content, content_size):
     if content_size >= 16:
         content_size_to_encrypt = content_size - (content_size % 16)
@@ -76,31 +70,33 @@ def encrypt_file(filecontent, filename, time):
     return cipher
 
 def decrypt_file(filecontent, filename, time):
-    filename = filename.replace(".[embrace@airmail.cc].embrace", "")
+    filename = filename.replace(CRYPTED_EXTENSION, "")
     IV = derive_IV_from_filename(filename)
     size_to_decrypt = (len(filecontent) / 16) * 16
     aes = AES.new(init_keys(time), AES.MODE_CBC, derive_IV_from_filename(filename))
     plain = aes.decrypt(filecontent[:size_to_decrypt]) + filecontent[size_to_decrypt:]
     return plain
 
-def test():
-    for i in range(100000):
-        test_content = "mqkjrzprlghplzrhkajhglkhrlkjgriuhgairhglkjh"
-        test_filename = "kikoo.txt"
-
-        locked_content = encrypt_file(test_content, test_filename, i)
-        locked_filename = test_filename + ".[embrace@airmail.cc].embrace"
-        locked_content += "a"*POSTFIX_SIZE
-
-        plain = decrypt_file(locked_content, locked_filename, i)
-        assert(plain == test_content)
-
 def entropy(string):
         prob = [ float(string.count(c)) / len(string) for c in dict.fromkeys(list(string)) ]
         entropy = - sum([ p * math.log(p) / math.log(2.0) for p in prob ])
         return entropy
 
-def try_unlock(filename, righttime=None, delta=1000, distance=entropy):
+def bruteforce_encryption_time(filecontent, basetime, filename, delta, distance):
+    closest = 2<<128
+    besttime = 0
+    start = filetime - delta/2
+    end = filetime + delta/2
+    print "[+] Trying every possible encryption time between %d and %d" % (start, end)
+    for time in range(start, end):
+        plain = decrypt_file(filecontent, filename, time)
+        dist = distance(plain)
+        if dist < closest:
+            besttime = time
+            closest = dist
+            print "\tTimestamp tested:", besttime, "entropy: ", closest, "decrypted file start: ", repr(plain[:30])
+
+def try_unlock_file(filename, decryptiontime=None, delta=None, distance=entropy):
     original_size = os.path.getsize(filename) - POSTFIX_SIZE
     with open(filename, 'rb') as f:
         if (original_size < ENCRYPTION_SIZE_LIMIT) or (os.path.splitext(filename) in SPECIAL_EXTENTIONS):
@@ -109,49 +105,52 @@ def try_unlock(filename, righttime=None, delta=1000, distance=entropy):
             filecontent = f.read(ENCRYPTION_SIZE_LIMIT)
     filetime = int(os.path.getmtime(filename))
     filesize = len(filecontent)
-    print "File name: %s, encrypted size: %s, last modification time: %s " % (filename, filesize, filetime)
-    
-    if righttime is not None:
-        return decrypt_file(filecontent, filename, righttime)
-    
-    print "[+] You did not provide the exact time of the attack "
-    closest = 2<<128
-    besttime = 0
-    for time in range(filetime - delta/2, filetime + delta/2):
-        plain = decrypt_file(filecontent, filename, time)
-        dist = distance(plain)
-        if dist < closest:
-            besttime = time
-            closest = dist
-            print "Timestamp tested:", besttime, "entropy: ", closest, repr(plain[:30])
-    print "The right time seems to be %s" % besttime
-    print "Use it to decrypt every other files in the same machine"
-    print_help()
+    print "[+] File name: %s, encrypted size: %s, last modification time: %s " % (filename, filesize, filetime)
+
+    if decryptiontime is not None and delta is None:
+        return decrypt_file(filecontent, filename, decryptiontime)
+
+    if delta is None:
+        delta = 1000
+
+    if decryptiontime is None:
+        print "[-] You did not provide the time of the attack."
+        print "[+] Trying the %d possible values arount the file's last modification date" % delta
+        basetime = filetime
+    else:
+        print "[+] Trying the %d possible values arount the provided date" % delta
+        basetime = decryptiontime
+
+    besttime = bruteforce_encryption_time(filecontent, basetime, filename, delta, distance)
+    print "[+] The encryption time seems to be %s" % besttime
+    print "[+] Use it to decrypt every other files in the same machine (cf. help)"
     return decrypt_file(filecontent, filename, besttime)
 
-if len(sys.argv) < 2:
-    print_help()
-    exit()
 
-filename = sys.argv[1]
-filename = os.path.abspath(filename)
+parser = argparse.ArgumentParser(description="Decrypt .embrace ransomware files", epilog="For this tool to work, the last 16 characters of the encrypted file's path (including the file's name, without '%s') must be the same as when the file was encrypted\r\nIf this condition is not met, only the 16 first bytes of the file at most will be destroyed. The rest of the file will be correctly decrypted." % CRYPTED_EXTENSION)
+parser.add_argument('file', nargs='+', help='file(s) to decrypt')
+parser.add_argument('--time', type=int, help='timestamp of the encryption (in seconds since Epoch), if known')
+parser.add_argument('--delta', type=int, help='number of seconds to bruteforce, around the provided timestamp, or the file\'s last modification date')
 
-if not filename.endswith(CRYPTED_EXTENSION):
-    print "File name should end with %s " % CRYPTED_EXTENSION
-    exit()
-    
-if len(sys.argv) >= 3:
-    decrypted_content = try_unlock(filename, righttime=int(sys.argv[2]))
-else:
-    decrypted_content = try_unlock(filename)
+args = parser.parse_args()
 
-new_filename = os.path.split(filename.replace(CRYPTED_EXTENSION, ""))
-new_filename = list(new_filename)
-new_filename[-1] = "decrypted_" + new_filename[-1]
-new_filename = os.sep.join(new_filename)
-with open(new_filename, "wb") as newf:
-    with open(filename, "rb") as oldf:
-        newf.write(decrypted_content)
-        oldf.seek(len(decrypted_content))
-        remaining_size = os.path.getsize(filename) - len(decrypted_content) - POSTFIX_SIZE
-        shutil.copyfileobj(oldf, newf, remaining_size)
+for filename in args.file:
+    filename = os.path.abspath(filename)
+    print "[+] Decrypting file %s" % filename
+    if not filename.endswith(CRYPTED_EXTENSION):
+        print "[-] File name %s should end with %s " % (filename, CRYPTED_EXTENSION)
+        print"Skipping..."
+        continue
+
+    decrypted_content = try_unlock_file(filename, righttime=args.time)
+
+    new_filename = os.path.split(filename.replace(CRYPTED_EXTENSION, ""))
+    new_filename = list(new_filename)
+    new_filename[-1] = "decrypted_" + new_filename[-1]
+    new_filename = os.sep.join(new_filename)
+    with open(new_filename, "wb") as newf:
+        with open(filename, "rb") as oldf:
+            newf.write(decrypted_content)
+            oldf.seek(len(decrypted_content))
+            remaining_size = os.path.getsize(filename) - len(decrypted_content) - POSTFIX_SIZE
+            shutil.copyfileobj(oldf, newf, remaining_size)
